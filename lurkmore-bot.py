582a42d207270ef5ea8114dbea6abab9217413a4
Konstantin Perikov <pericov.ko@gmail.com>
2017-09-15 19:27:09 +0300

    add abitility to update topic/pin message

--- lurkmore-bot.py
+++ lurkmore-bot.py
@@ -1,30 +1,47 @@
 from telegram.ext import Updater, CommandHandler, Job
 
+import telegram
 import sys
+import requests
+import bs4
 
 def help(bot, update):
     update.message.reply_text(text="""
         <b>Lurkmore Bot Commands:</b>
+        
+        If you want to get full functionality of this bot, you need to enable him as an administrator
 
         /help - show this help
+        /make_chat_great_again - update the chat with random Lurkmore page
         """, parse_mode='HTML')
 
 
+def updateChat(bot, update):
+    r = requests.get('http://lurkmore.co/Служебная:Random')
+    html = bs4.BeautifulSoup(r.text, "html.parser")
+    title = html.title.text.replace("Lurkmore", "").replace("—", "").strip()
+    chat_id = update.message.chat.id
+    print(title)
+    print(r.url)
+    print(chat_id)
+    try:
+        flag = bot.set_chat_title(chat_id, title)
+        msg = bot.send_message(chat_id, r.url)
+        print(msg.message_id)
+        print(html.find("img", {"class": "thumbborder"}))
+        x = "http:" + html.find("img", {"class": "thumbborder"})['src']
+        print(x)
+        bot.set_chat_photo(update.message.chat.id, photo=x)
+        bot.pin_chat_message(chat_id, msg.message_id)
+    except telegram.TelegramError as te:
+        print("Telegram error was raised during workflow {}").format(te)
 
 
 def main(argv):
-    # if len(argv) != 1:
-    #     print('Expected usage of script:')
-    #     print('python3 bot.py TelegramAPIKey')
-    #     sys.exit(2)
-    #     telegram_api_key = argv
-    # try:
-    #     updater = Updater(telegram_api_key)
-    # except Exception:
-    #     print("Provided params are incorrect. Couldn't connect to Telegram")
-    updater = Updater("")
+    updater = Updater("ENTER TELEGRAM BOT TOKEN")
 
     updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('make_chat_great_again', updateChat))
     updater.start_polling()
     updater.idle()
 
