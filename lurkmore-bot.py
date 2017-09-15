819045e399d9fa360d1edcebc810aba6992c9862
Konstantin Perikov <pericov.ko@gmail.com>
2017-09-15 20:09:15 +0300

    add some additional commands, add timeout on update operation

--- lurkmore-bot.py
+++ lurkmore-bot.py
@@ -4,6 +4,9 @@ import telegram
 import sys
 import requests
 import bs4
+import shutil
+from io import BytesIO
+import datetime
 
 def help(bot, update):
     update.message.reply_text(text="""
@@ -12,38 +15,59 @@ def help(bot, update):
         If you want to get full functionality of this bot, you need to enable him as an administrator
 
         /help - show this help
-        /make_chat_great_again - update the chat with random Lurkmore page
+        /updateChat - update the chat with random Lurkmore page
+        /setup - you could ask your bot to update your chat at specific time or every X units of time
         """, parse_mode='HTML')
 
+def setup(bot, update):
+    update.message.reply_text(text="""
+        <b>Setting of the Lurkmore Bot</b>
+
+        TBD
+        """, parse_mode='HTML')
+
+one_hour = datetime.timedelta(hours=1.0)
+last_time = datetime.datetime.utcnow()
 
 def updateChat(bot, update):
-    r = requests.get('http://lurkmore.co/Служебная:Random')
-    html = bs4.BeautifulSoup(r.text, "html.parser")
-    title = html.title.text.replace("Lurkmore", "").replace("—", "").strip()
     chat_id = update.message.chat.id
-    print(title)
-    print(r.url)
-    print(chat_id)
-    try:
-        flag = bot.set_chat_title(chat_id, title)
-        msg = bot.send_message(chat_id, r.url)
-        print(msg.message_id)
-        print(html.find("img", {"class": "thumbborder"}))
-        x = "http:" + html.find("img", {"class": "thumbborder"})['src']
-        print(x)
-        bot.set_chat_photo(update.message.chat.id, photo=x)
-        bot.pin_chat_message(chat_id, msg.message_id)
-    except telegram.TelegramError as te:
-        print("Telegram error was raised during workflow {}").format(te)
+    if datetime.datetime.utcnow() - last_time < one_hour:
+        bot.send_message(chat_id, text="I can't let you do that now. Remaining time is {}".format(datetime.datetime.utcnow() - last_time))
+    else:
+        r = requests.get('http://lurkmore.co/Служебная:Random')
+        html = bs4.BeautifulSoup(r.text, "html.parser")
+        title = html.title.text.replace("Lurkmore", "").replace("—", "").strip()
+        print(title)
+        print(r.url)
+        print(chat_id)
+        try:
+            bot.set_chat_title(chat_id, title)
+            msg = bot.send_message(chat_id, r.url)
+            print(msg.message_id)
+            find = html.find("div", {"class": "thumbinner"}).find("img")['src']
+            x = "http:" + find
+            print(x)
+            picture = requests.get(x, stream=True)
+            with open('logo.png', 'wb') as out_file:
+                shutil.copyfileobj(picture.raw, out_file)
+            del picture
+            raw_bytes = BytesIO(open('logo.png', 'rb').read())
+            bot.set_chat_photo(update.message.chat.id, photo=raw_bytes)
+            bot.pin_chat_message(chat_id, msg.message_id)
+        except telegram.TelegramError as te:
+            print(te)
 
 
 def main(argv):
-    updater = Updater("ENTER TELEGRAM BOT TOKEN")
+    updater = Updater("")
 
     updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('make_chat_great_again', updateChat))
+    updater.dispatcher.add_handler(CommandHandler('start', help))
+    updater.dispatcher.add_handler(CommandHandler('updateChat', updateChat))
+    updater.dispatcher.add_handler(CommandHandler('setup', setup))
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
