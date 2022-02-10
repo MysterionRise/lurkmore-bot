178d16687126a5e7052c3a66e0d6cc70fab8972a
Konstantin Perikov <pericov.ko@gmail.com>
2022-02-10 14:21:25 +0000

    update the formatting, add black&isort

--- lurkmore-bot.py
+++ lurkmore-bot.py
@@ -1,62 +1,69 @@
-from telegram.ext import Updater, CommandHandler
-from telegram.ext.dispatcher import run_async
-
-import sys
-import requests
-import bs4
 import shutil
+import sys
 from io import BytesIO
-from PIL import Image
 from urllib.parse import unquote
+
+import bs4
 import numpy as np
+import requests
+from PIL import Image
+from telegram.ext import CommandHandler, Updater
+from telegram.ext.dispatcher import run_async
 
 
 @run_async
 def help(update, context):
     chat_id = update.message.chat.id
-    context.bot.send_message(chat_id, text="""
+    context.bot.send_message(
+        chat_id,
+        text="""
         <b>Lurkmore Bot Commands:</b>
         
         If you want to get full functionality of this bot, you need to enable him as an administrator
 
         /help - show this help
         /updateChat - update the chat with random Lurkmore page
-        """, parse_mode='HTML')
+        """,
+        parse_mode="HTML",
+    )
 
 
 prev_titles = set()
+
 
 @run_async
 def updateChat(update, context):
     try:
         chat_id = update.message.chat.id
-        r = requests.get('http://lurkmore.to/Служебная:Random')
+        r = requests.get("http://lurkmore.to/Служебная:Random")
         html = bs4.BeautifulSoup(r.text, "html.parser")
         title = html.title.text.replace("Lurkmore", "").replace("—", "").strip()
         while title in prev_titles or "Копипаста" in title:
-            r = requests.get('http://lurkmore.to/Служебная:Random')
+            r = requests.get("http://lurkmore.to/Служебная:Random")
             html = bs4.BeautifulSoup(r.text, "html.parser")
             title = html.title.text.replace("Lurkmore", "").replace("—", "").strip()
         prev_titles.add(title)
         unquoted_titile = unquote(r.url)
         print(unquoted_titile)
-        msg = context.bot.send_message(chat_id, unquoted_titile, parse_mode='HTML')
+        msg = context.bot.send_message(chat_id, unquoted_titile, parse_mode="HTML")
         context.bot.pin_chat_message(chat_id, msg.message_id)
         context.bot.set_chat_title(chat_id, title)
         find_all = html.findAll("img", {"class": "thumbimage"})
-        find = find_all[0]['src'].replace("thumb", "").rsplit('/', 1)[0]
+        find = find_all[0]["src"].replace("thumb", "").rsplit("/", 1)[0]
         x = "http:" + find
         print(x)
         picture = requests.get(x, stream=True)
-        with open('logo.png', 'wb') as out_file:
+        with open("logo.png", "wb") as out_file:
             shutil.copyfileobj(picture.raw, out_file)
         del picture
-        im = Image.open('logo.png')
+        im = Image.open("logo.png")
         sqrWidth = np.ceil(np.sqrt(im.size[0] * im.size[1])).astype(int)
         im_resize = im.resize((sqrWidth, sqrWidth))
-        im_resize.save('output.png')
-        raw_bytes = BytesIO(open('output.png', 'rb').read())
-        context.bot.set_chat_photo(update.message.chat.id, photo=raw_bytes, timeout=3000)
+        im_resize.save("output.png")
+        raw_bytes = BytesIO(open("output.png", "rb").read())
+        context.bot.set_chat_photo(
+            update.message.chat.id, photo=raw_bytes, timeout=3000
+        )
     except Exception as e:
         print(e)
 
@@ -64,9 +71,9 @@ def updateChat(update, context):
 def main(argv):
     updater = Updater("", use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('start', help))
-    updater.dispatcher.add_handler(CommandHandler('updateChat', updateChat))
+    updater.dispatcher.add_handler(CommandHandler("help", help))
+    updater.dispatcher.add_handler(CommandHandler("start", help))
+    updater.dispatcher.add_handler(CommandHandler("updateChat", updateChat))
     updater.start_polling()
     updater.idle()
 
